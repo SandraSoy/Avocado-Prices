b9e93940911ba496d47fe1a3cb2779003aa35cb1
Sandra C. Soy <142498949+SandraSoy@users.noreply.github.com>
2023-08-17 18:26:45 +0300

    Update Avocado.py

--- Avocado.py
+++ Avocado.py
@@ -2,8 +2,8 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+#import matplotlib.pyplot as plt
+#import seaborn as sns
 
 #Title
 st.title('Avocado Analytics Web App')
